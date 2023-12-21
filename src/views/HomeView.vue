7861689e4019d78f455032bf7176fb6fdcde8bfe
= <=>
2023-12-21 01:31:09 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+function playSound() {
+  const sound = (new Audio(require('@/assets/ring.mp3')).play())
+}
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <button @click="playSound" class="bg-red-400 text-white h-32 w-52 border-none text-3xl rounded-2xl hover:bg-red-600 transition">Kuscheln</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
